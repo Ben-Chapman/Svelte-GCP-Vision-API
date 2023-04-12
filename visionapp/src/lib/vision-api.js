d0892446fd6985f672aba1cc22962f4938f2ef35
Ben Chapman <5124569+Ben-Chapman@users.noreply.github.com>
2023-04-11 21:01:00 -0400

    Vision API working

--- visionapp/src/lib/vision-api.js
+++ visionapp/src/lib/vision-api.js
@@ -1,15 +1,86 @@
-import vision from '@google-cloud/vision';
+import { testImage } from "./test-image";
 
-export async function processImage() {
-	// Creates a client
-	const client = new vision.ImageAnnotatorClient();
+export async function annotateImage(sourceImage) {
+  let postData = {
+    requests: [
+      {
+        image: {
+          // The source image needs to be a base64 encoded string
+          content: testImage,
+          // content: convertToBase64(sourceImage)
+        },
+        features: [
+          {
+            type: "LABEL_DETECTION",
+            maxResults: 3,
+            model: "builtin/latest",
+          },
+          {
+            type: "TEXT_DETECTION",
+            maxResults: 1,
+            model: "builtin/latest",
+          },
+          {
+            type: "FACE_DETECTION",
+            maxResults: 1,
+            model: "builtin/latest",
+          },
+          {
+            type: "LANDMARK_DETECTION",
+            maxResults: 10,
+            model: "builtin/latest",
+          },
+          {
+            type: "LOGO_DETECTION",
+            maxResults: 10,
+            model: "builtin/latest",
+          },
+        ],
+      },
+    ],
+  };
 
-	// Performs label detection on the image file
-	const [result] = await client.labelDetection('/home/benchapman/Downloads/nyc.jpg');
-	const labels = result.labelAnnotations;
-	console.log('Labels:');
-	labels.forEach((label) => console.log(label.description));
-	return labels;
+  const visionApiUrl = "https://vision.googleapis.com/v1/images:annotate";
+
+  try {
+    // Ensure an env variable was set, containing the GCP auth token
+    var authToken = import.meta.env.VITE_AUTH_TOKEN;
+    var gcpBillingProject = import.meta.env.VITE_GCP_BILLING_PROJECT;
+  } catch (envVarError) {
+    console.error("A required environment variable was not set.");
+  }
+  try {
+    const response = await fetch(visionApiUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "X-Goog-User-Project": gcpBillingProject,
+        Authorization: `Bearer ${authToken}`,
+      },
+      body: JSON.stringify(postData),
+    });
+
+    const result = await response.json();
+    console.log(result.error);
+
+    if (!response.ok) {
+      throw new Error(`An error occured with the Vision API: ${result.error.message}`);
+    }
+    console.log("Success:", result);
+    return JSON.stringify(result.responses);
+  } catch (fetchError) {
+    throw fetchError;
+  }
 }
 
-// processImage();
+function convertToBase64(file) {
+  const reader = new FileReader();
+  reader.readAsDataURL(file);
+  return reader.result;
+  // reader.onload = function () {
+  //   console.log(reader.result);
+  // };
+  // reader.onerror = function (error) {
+  //   console.log('Error: ', error);
+  // };
+}
