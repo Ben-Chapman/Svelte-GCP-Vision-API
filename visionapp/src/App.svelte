96fae0cff235213ac6f0f3579caefadcbb487e02
Ben Chapman <5124569+Ben-Chapman@users.noreply.github.com>
2023-04-24 11:10:50 -0400

    Display image filename after upload

--- visionapp/src/App.svelte
+++ visionapp/src/App.svelte
@@ -5,7 +5,10 @@
 </script>
 
 <main>
-  <!-- <Flex justify="around" align="center" class="flexbox"> -->
+  <Flex justify="left">
+    <img src="../visionapp-logo.png" alt="The VisionApp Logo" class="logo" />
+  </Flex>
+  <Flex justify="around" align="center" class="flexbox">
     <div class="file-drop-section">
       <Dropzone />
     </div>
@@ -13,32 +16,33 @@
     <div class="annotation-section">
       <Annotations />
     </div>
-  <!-- </Flex> -->
+  </Flex>
 </main>
 
 <style>
-  /* .file-drop-section {
-    background-color: #ffffff24;
-    border-radius: 10px;
+  .file-drop-section {
     flex: 2;
     height: 100%;
     align-self: center;
+    background-color: #ffffff24;
+    border-radius: 10px;
   }
+
   .annotation-section {
     flex: 1;
     height: 100%;
     overflow-y: scroll;
-  } */
+  }
   /* Hide the scrollbar in Chrome */
   :global(.annotation-section::-webkit-scrollbar) {
     display: none;
   }
 
-  /* :global(body) {
+  :global(body) {
     background-color: #3d3d3d;
     color: #ffffff;
     font-family: "Encode Sans Condensed", sans-serif;
-  } */
+  }
   :global(.flexbox) {
     width: 80vw;
     height: 80vh;
