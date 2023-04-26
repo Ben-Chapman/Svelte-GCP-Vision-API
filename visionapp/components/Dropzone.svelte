23fa4d28f8a39a3ae14d092130c0b62a25404bb1
Ben Chapman <5124569+Ben-Chapman@users.noreply.github.com>
2023-04-26 14:06:53 -0400

    Adding image styling and optimizing the dropzone configurations

--- visionapp/components/Dropzone.svelte
+++ visionapp/components/Dropzone.svelte
@@ -1,10 +1,13 @@
 <script>
+  import Dropzone from "dropzone";
   import { onMount } from "svelte";
+
   import { imageData } from "../src/lib/stores";
 
-  import Dropzone from "dropzone";
-
   let filename = "";
+  let imageDropped = false;
+  let haveThumbnail = false;
+
   function createDropzone() {
     let myDropzone = new Dropzone(document.body, {
       url: "#",
@@ -17,18 +20,35 @@
       autoProcessQueue: false,
       previewTemplate: document.getElementById("custom-template").innerHTML,
     });
+    /**
+     * This project is desgined to accept only one image at a time. So when an
+     * image is dropped and before it's uploaded, remove any previously uploaded
+     * images from the dropzone. If no previous images are present, the remove
+     * operation has no effect.
+     */
+    myDropzone.on("drop", function () {
+      imageDropped = true;
+      console.log("Drop detected, removing files");
+      filename = "";
+      myDropzone.removeAllFiles();
+    });
 
     myDropzone.on("addedfile", function (file) {
-      console.log(`Added here ${file.upload.filename}`);
+      console.log(`A new image has been dropped: ${file.upload.filename}`);
       const reader = new FileReader();
       reader.onload = () => {
-        filename = file.upload.filename;
         imageData.set(reader.result.split(",")[1]);
       };
       reader.onabort = () => console.log("file reading was aborted");
       reader.onerror = () => console.log("file reading has failed");
 
       reader.readAsDataURL(file);
+    });
+
+    myDropzone.on("thumbnail", function (file) {
+      console.log(`Thumbnail generated for ${file.upload.filename}`);
+      filename = file.upload.filename;
+      haveThumbnail = true;
     });
   } // end of dropzone configuration
 
@@ -37,14 +57,16 @@
 
 <main>
   <div class="preview-container" />
-  {#if filename === ""}
+  {#if !imageDropped}
     <h1>Drop image here!</h1>
   {:else}
     <p>{filename}</p>
   {/if}
   <div id="custom-template">
     <div class="dz-preview dz-file-preview">
-      <div class="dz-image"><img class="image-preview" data-dz-thumbnail /></div>
+      <div class="dz-image">
+        <img class="image-preview" data-dz-thumbnail />
+      </div>
       <div class="dz-progress">
         <span class="dz-upload" data-dz-uploadprogress />
       </div>
@@ -58,7 +80,10 @@
     align-self: flex-end;
   }
   .image-preview {
-    /* Round the upper corners of the preview image to match it's container */
-    border-radius: 10px 10px 0 0;
+    /* Scale portrait images to fit within their container */
+    max-height: 83ch;
+    max-width: 95%;
+    padding-top: 1.3rem;
+    filter: drop-shadow(2px 2px 18px rgba(0, 0, 0, 0.7));
   }
 </style>
